0a605cc513eb9c752d4b146eabc76b6786045864
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-10 16:32:49 +0700

    feat (module): handle connect db, handle interceptor, handle filter for application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,48 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { ErrorInterceptor } from './interceptors/handleError.interceptor';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL, {
+      retryAttempts: 10, // thử kết nối 10 lần nếu thất bại
+      retryDelay: 5000, //chờ 5s trước khi thử kết nối lại
+      connectionFactory: (db) => {
+        const logger = new Logger('DATABASE');
+        db.on('connected', () => {
+          logger.verbose(
+            `Kết nối cơ sở dữ liệu ${process.env.DATABASE_URL} thành công`,
+          );
+        });
+        db.on('error', (error) => {
+          logger.error(`Kết nối cơ sở dữ liệu thất bại: ${error.message}`);
+        });
+        db._events.connected();
+        return db;
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}

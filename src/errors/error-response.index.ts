d1a355ebe18f8979e95f23422460480f2c5d46c3
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-14 17:46:40 +0700

    feat (error): add error model for swagger api doc

--- src/errors/error-response.index.ts
+++ src/errors/error-response.index.ts
@@ -4,10 +4,23 @@ import {
   UsernameExistsError,
   InvalidRoleError,
 } from 'src/modules/auth/dto/error-response.dto';
-
+import {
+  ForbiddenChangeAdminRoleAndPermissionError,
+  PermissionIdNotFoundError,
+  RoleAndSubjectAlreadyExistError,
+  RoleIdNotFoundError,
+  RoleNameAlreadyExistsError,
+  RoleNameIsNotEmptyError,
+} from 'src/modules/permission/dto/error-response.dto';
 export const errorResponses = [
   EmailExistsError,
   InvalidEmailError,
   UsernameExistsError,
   InvalidRoleError,
+  ForbiddenChangeAdminRoleAndPermissionError,
+  PermissionIdNotFoundError,
+  RoleAndSubjectAlreadyExistError,
+  RoleIdNotFoundError,
+  RoleNameAlreadyExistsError,
+  RoleNameIsNotEmptyError,
 ];

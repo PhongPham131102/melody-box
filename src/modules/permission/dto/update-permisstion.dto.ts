3150546750bd1601b3e9467f2fb63cff3564ce58
PhongPham131102 <phongpham131102@gmail.com>
2024-10-14 23:13:37 +0700

    feat (dto): add description docs api for update permission dto

--- src/modules/permission/dto/update-permisstion.dto.ts
+++ src/modules/permission/dto/update-permisstion.dto.ts
@@ -1,13 +1,26 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { ActionEnum } from 'src/enums/index.enum';
 
 export class UpdatePermisstionDto {
+  @ApiPropertyOptional({
+    description: 'List of allowed actions for this permission',
+    enum: ActionEnum,
+    isArray: true,
+    example: ['CREATE', 'READ'],
+  })
   @IsOptional()
   @IsEnum(ActionEnum, {
     each: true,
-    message: 'Action Must A Type Of Action Enum',
+    message: 'Action Must Be A Type Of Action Enum',
   })
   action: ActionEnum[];
+
+  @ApiPropertyOptional({
+    description: 'The subject of this permission',
+    type: String,
+    example: 'user',
+  })
   @IsOptional()
   @IsString({ message: 'Subject Must Be A String' })
   subject: string;
